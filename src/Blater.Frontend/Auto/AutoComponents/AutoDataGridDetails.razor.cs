9391a99e03a28adee198dbd9f9514deff57f9e50
breno <breno@autosystem.top>
2024-06-28 16:07:27 -0300

    ✨ feat(ui): refactor Auto components for better separation of concerns
    - Moved code-behind logic to separate .cs files for clarity
    - Improves maintainability and readability of Razor components
    - Removes unnecessary usings from Razor files
    
    Refactored components:
    - AutoDataGridDetails
    - AutoInfoCard
    - AutoForm
    - AutoDetails
    
    Benefits:
    - Enhanced code organization 🎨
    - Easier debugging and testing 🛠️
    - Improved separation of concerns 📚
    
    Closes #245

---
+++ src/Blater.Frontend/Auto/AutoComponents/AutoDataGridDetails.razor.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Blater.Enumerations.AutoModel;
+using Blater.Models.Bases;
+
+namespace Blater.Frontend.Auto.AutoComponents;
+
+[SuppressMessage("Usage", "CA2252:Esta API requer a aceitação de recursos de visualização")]
+public partial class AutoDataGridDetails<TIem, TList> 
+    where TIem : BaseDataModel
+    where TList : List<TIem>
+{
+    public int Priority => 1000;
+
+    public BaseAutoComponentTypeEnumeration ComponentType => AutoComponentType.AutoDataGrid;
+}
---
+++ src/Blater.Frontend/Auto/AutoForm.razor.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Blater.Frontend.Auto.AutoBuilders;
+using Blater.Frontend.Models;
+using Blater.Frontend.Services;
+using Blater.Models;
+using Blater.Models.Bases;
+using Microsoft.AspNetCore.Components;
+
+namespace Blater.Frontend.Auto;
+
+[SuppressMessage("Usage", "CA2252:Esta API requer a aceitação de recursos de visualização")]
+public partial class AutoForm<T> where T : BaseDataModel
+{
+    private readonly string _typeName = typeof(T).Name;
+
+    [Inject] public LocalizationService LocalizationService { get; set; } = null!;
+
+    [Parameter]
+    public BlaterId? Id { get; set; }
+
+    [Parameter]
+    public T? Model { get; set; }
+
+    [Parameter]
+    public FindPropertiesAttributeOptions<T>? Options { get; set; }
+    
+    [Parameter]
+    public Func<T, Task<bool>>? BeforeUpsert { get; set; }
+
+    private AutoFormBuilder<T> FormBuilder { get; set; } = null!;
+}
