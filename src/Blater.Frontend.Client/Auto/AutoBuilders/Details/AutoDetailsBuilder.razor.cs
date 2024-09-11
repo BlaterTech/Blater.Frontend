e8b930452fce39f8b6f39446859df9e1cfec9dc9
breno <breno@autosystem.top>
2024-09-11 14:23:15 -0300

    ✨ feat(ui): add AutoDetailsBuilder component for detailed auto views
    Introduced a new component for displaying detailed views of auto models.
    This enhancement improves the modularity and reusability of the UI components.

--- src/Blater.Frontend.Client/Auto/AutoBuilders/Details/AutoDetailsBuilder.razor.cs
+++ src/Blater.Frontend.Client/Auto/AutoBuilders/Details/AutoDetailsBuilder.razor.cs
@@ -4,12 +4,10 @@ using Blater.Models.Bases;
 
 namespace Blater.Frontend.Client.Auto.AutoBuilders.Details;
 
-public class AutoDetailsBuilder<T> : BaseAutoComponentBuilder<T> where T : BaseDataModel
+public partial class AutoDetailsBuilder<T> : BaseAutoComponentBuilder<T> where T : BaseDataModel
 {
     public override AutoComponentDisplayType DisplayType { get; set; } = AutoComponentDisplayType.Details;
     public override bool HasLabel { get; set; }
-
-
     protected override void LoadModelConfig()
     {
         throw new NotImplementedException();
